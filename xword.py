6c4ba2442b017cae6acae23dde09c83e29f0b719
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-04-13 15:15:54 -0400

    fin

--- xword.py
+++ xword.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
+import re
 
-__author__ = "???"
+__author__ = "Cedric Mulvihill"
 
-# YOUR HELPER FUNCTION GOES HERE
+"""We need a function that takes a string with missing characters
+and a list of all possible words"""
+def match_words(string, library):
+    #to format the string into individual letters and re
+    string_formatter = []
+    #list we will return with matching words
+    match_list = []
+    """Iterate through the string to seperate the letters
+    from the missing characters, replacing the missing 
+    characters with regex expression for letters in range
+    from a to z"""
+    for i in string:
+        if i == ' ':
+            string_formatter.append('[a-z]')
+        else:
+            string_formatter.append(i)
+    #String formatted for regex
+    f_string = ''.join(string_formatter)
+    """Iterating through the dictionary to find matches for
+    the given string"""
+    for w in library:
+        if re.match(f_string, w):
+            match_list.append(w)
+    return match_list
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
 
-    test_word = raw_input(
+    test_word = input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
-
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    for i in match_words(test_word, words):
+        print(i)
 
 
 if __name__ == '__main__':
